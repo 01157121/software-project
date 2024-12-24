45c2b0b8e6aeafafb4cff7c578d1dee5870eaa39
雪人愛情 <s99006311@gmail.com>
2024-12-25 01:37:45 +0800

    Update build.gradle.kts

--- project_android/build.gradle.kts
+++ project_android/build.gradle.kts
@@ -3,29 +3,5 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
-    id("com.android.application")
-    kotlin("android")
 }
-android {
-    compileSdk = 33
 
-    defaultConfig {
-        applicationId = "com.example.project_android"
-        minSdk = 21
-        targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-
-    lint {
-        baseline = file("lint-baseline.xml") // 設置基線文件
-        abortOnError = false // 禁止 Lint 問題中止構建
-    }
-}
